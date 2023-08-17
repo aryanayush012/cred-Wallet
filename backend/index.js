66564055e9d6ce3dc636a0e814da654bd669f9c0
Ayush Aryan <aryanayush012@gmail.com>
2023-08-18 02:45:57 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,14 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.use(express.static(path.join(__dirname, "../build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build/index.html"));
+// });
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(process.env.PORT || port, () => {
